aad40ae6453ccf3d2551b28afc81a65b56fe2059
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-21 19:39:55 -0400

    Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+Return True if all boxes can be opened, else return False
+"""
+
 
 def canUnlockAll(boxes):
     new_list = [0]
 
     for index in new_list:
         for num in boxes[index]:
-            if num not in new_list and num < len(boxes) and isinstance(num, int) and num >= 0:
+            if num not in new_list and num < len(boxes) \
+               and isinstance(num, int) and num >= 0:
                 new_list.append(num)
 
     return(bool(len(new_list) == len(boxes)))
