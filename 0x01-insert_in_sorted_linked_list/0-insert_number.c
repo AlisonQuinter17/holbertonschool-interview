77a3dcb2606a531eec379f106048c1e0eeaf8ddb
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-29 13:36:13 -0400

    Done

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -16,12 +16,13 @@ listint_t *insert_node(listint_t **head, int number)
 	{
 		return (NULL);
 	}
-
 	new_node->n = number;
 
 	if (!*head)
 	{
-		return (NULL);
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
 	}
 	else
 	{
