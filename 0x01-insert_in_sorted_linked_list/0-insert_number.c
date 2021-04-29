745bb4fc573358fe2f649440214e523eb9f4f97e
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-29 12:24:15 -0500

    Done!

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -8,5 +8,33 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+    listint_t *new_node = NULL;
+    listint_t *current = *head;
 
+    new_node = malloc(sizeof(listint_t));
+    if (!new_node)
+    {
+        return (NULL);
+    }
+
+    new_node->n = number;
+
+    if (!*head)
+    {
+        return (NULL);
+    }
+    else
+    {
+        while (current->next != NULL)
+        {
+            if (current->next->n > new_node->n)
+            {
+                new_node->next = current->next;
+                current->next = new_node;
+                break;
+            }
+            current = current->next;
+        }
+    }
+    return (new_node);
 }
