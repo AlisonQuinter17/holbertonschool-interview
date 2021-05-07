77e4b265e347ba80845409f34db8b9ce603b226e
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-06 23:38:02 -0500

    Update

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - Creates a binary tree node.
+ *
+ * @root: Double pointer to the root node of the Heap.
+ * @value: Value to store in the node to be inserted.
+ * Return: A pointer to the inserted node, or NULL on failure.
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+
+}
