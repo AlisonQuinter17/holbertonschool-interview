95768313335a4ecd840b5d38bec429890f072f59
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-10 17:00:09 -0500

    Empty

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+- Minimum Operations
+- Returns an integer
+"""
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file. 
+    """
