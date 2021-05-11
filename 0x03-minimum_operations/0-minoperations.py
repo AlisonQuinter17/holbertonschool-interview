f2aaceeb0bc6a52cf6211c5e4abf87c3d376b073
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-11 14:38:13 -0500

    Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,8 +4,22 @@
 - Returns an integer
 """
 
+
 def minOperations(n):
     """
     Calculates the fewest number of operations needed
     to result in exactly n H characters in the file. 
     """
+    counter = 0
+    h = 2
+
+    if type(n) != int or n <= 0:
+        return (0)
+
+    while (n != 1):       
+        if n % h:
+            h += 1
+        else:
+            n /= h
+            counter += h
+    return (counter)
