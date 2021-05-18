4c1a978f2385b86c6d13e7c9dbbb2a9e5c21f180
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-18 13:05:46 -0400

    Sandpiles

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,12 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles.
+ *
+ * @grid1: Sandpile Stable.
+ * @grid2: Sandpile Stable.
+ * Return: 0.
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
