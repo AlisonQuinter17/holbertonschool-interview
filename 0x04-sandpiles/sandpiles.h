34afd9fc3548c044a6510c50904ed3ca3580c6ae
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-20 18:43:14 -0400

    Sandpiles

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,8 +4,6 @@
 /* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
