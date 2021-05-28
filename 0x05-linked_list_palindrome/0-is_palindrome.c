d020c5d9d12f695c263b1ddec4ef5c1206592bd3
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-28 01:21:35 -0400

    Super Efficient

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -7,17 +7,17 @@
  */
 int is_palindrome(listint_t **head)
 {
-	return (detection(*head, head));
+	return (detection(head, *head));
 }
 
 /**
  * detection - Detect if a list is palindrome.
- * @head: Pointer to the head node of the list, far left.
- * @aux: Double pointer to copy of head, far right.
+ * @head: Double Pointer to the head node of the list, far left.
+ * @aux: Pointer to copy of head, far right.
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
-int detection(listint_t *head, listint_t **aux)
+int detection(listint_t **head, listint_t *aux)
 {
-	return ((!head) || (detection(head->next, aux) && head->n == (*aux)->n ?
-			    ((*aux) = (*aux)->next), 1 : 0));
+	return ((!aux) || (detection(head, aux->next) && (*head)->n == aux->n ?
+			   ((*head) = (*head)->next), 1 : 0));
 }
