adc91809d0d38889c0a24ea71b9337ec4455d617
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-03 16:07:24 -0500

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     total_size = 0
+    count = 1
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_stats():
@@ -12,20 +13,16 @@ if __name__ == "__main__":
             if codes[key]:
                 print("{}: {}".format(key, codes[key]))
 
-    i = 1
     try:
         for line in sys.stdin:
             tokens = line.split(" ")
             total_size += int(tokens[-1])
             code = int(tokens[-2])
-
             if code in codes:
                 codes[code] += 1
-
-            if i % 10 == 0:
+            if count % 10 == 0:
                 print_stats()
-
-            i += 1
+            count += 1
     except KeyboardInterrupt:
         print_stats()
         raise
