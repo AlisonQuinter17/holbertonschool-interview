fd22238790e3dc647485d9088c7027acfd9b174f
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-03 15:59:40 -0500

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,16 +4,15 @@ import sys
 
 if __name__ == "__main__":
     size = 0
-    i = 0
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size))
-        for key in sorted(codes.keys()):
-            if codes[key]:
-                print("{}: {}".format(key, codes[key]))
+        print("File size: {:d}".format(size))
+        for key, val in sorted(codes.keys()):
+            if codes[key] != 0:
+                print("{:s}: {:d}".format(key, codes[key]))
 
+    i = 1
     try:
         for line in sys.stdin:
             tokens = line.split(" ")
