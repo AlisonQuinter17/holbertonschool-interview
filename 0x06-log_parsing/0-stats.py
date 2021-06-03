a3f5b5884cd0d7720699ebb4ed62ee2eb5ecdcb7
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-03 16:35:42 -0500

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,25 +5,26 @@ import sys
 if __name__ == "__main__":
     total_size = 0
     count = 1
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_statistics():
         print("File size: {}".format(total_size))
-        for key, value in sorted(codes.items()):
+        for key, value in sorted(status_codes.items()):
             if (value):
                 print("{}: {}".format(key, value))
 
     try:
         for line in sys.stdin:
             tokens = line.split(" ")
-            total_size += int(tokens[-1])
-            code = int(tokens[-2])
+            if len(tokens) > 2:
+                total_size += int(tokens[-1])
+                code = int(tokens[-2])
 
-            if code in codes:
-                codes[code] += 1
+                if code in status_codes:
+                    status_codes[code] += 1
             if count % 10 == 0:
                 print_statistics()
-            count += 1
+            count += 1 
     except Exception:
         pass
     finally:
