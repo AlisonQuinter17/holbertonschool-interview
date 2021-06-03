b3af8d2726b0cdc1bd289b7f7fbbe00cb6959624
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-03 16:48:04 -0500

    Log Parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,11 +5,11 @@ import sys
 if __name__ == "__main__":
     total_size = 0
     count = 1
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_statistics():
         print("File size: {}".format(total_size))
-        for key, value in sorted(status_codes.items()):
+        for key, value in sorted(codes.items()):
             if (value):
                 print("{}: {}".format(key, value))
 
@@ -18,8 +18,9 @@ if __name__ == "__main__":
             tokens = line.split(" ")
             total_size += int(tokens[-1])
             code = int(tokens[-2])
-            if code in status_codes:
-                status_codes[code] += 1
+
+            if code in codes:
+                codes[code] += 1
             if count % 10 == 0:
                 print_statistics()
             count += 1
