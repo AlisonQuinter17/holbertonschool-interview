2d696cd030db3d36944f299ae18235c652ad601d
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-11 12:09:37 -0400

    UPdate

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-'''Module for log parsing script.'''
+'''Module for script that reads stdin line by line and computes metrics.'''
 import sys
 
 if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    def check_match(line):
-        '''Checks for regexp match in line.'''
+    total_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def reads_line(line):
         try:
             line = line[:-1]
             words = line.split(" ")
-            size[0] += int(words[-1])
+            total_size[0] += int(words[-1])
             code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
+            if code in status_codes:
+                status_codes[code] += 1
         except:
             pass
 
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
+    def print_statistics():
+        ''' Print Statistics. '''
+        print("File size: {}".format(total_size[0]))
+        for value in sorted(status_codes.keys()):
+            if (status_codes[value]):
+                print("{}: {}".format(value, status_codes[value]))
+
     i = 1
     try:
         for line in sys.stdin:
-            check_match(line)
+            reads_line(line)
             if i % 10 == 0:
-                print_stats()
-            i += 1
+                print_statistics()
+        i = i + 1
     except KeyboardInterrupt:
-        print_stats()
+        print_statistics()
         raise
-    print_stats()
+    print_statistics()
